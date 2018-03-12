be3d5698fedce236a8137798710d96b010f79e0d
jiangxs <songxjxs@yeah.net>
2018-03-12 14:48:57 +0800

    增加LeetCode第一题：Two Sum(easy)

--- src/com/jxs/Sum.java
+++ src/com/jxs/Sum.java
@@ -1,11 +1,22 @@
 package com.jxs;
 
+import org.junit.Test;
+
 /**
  * Created by jiangxs on 2018/3/12.
  */
 public class Sum {
 
-    public static void main(String[] args) {
+    @Test
+    public int[] toSum(int[] nums, int target) {
 
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
     }
 }
