f19425ce8c38e29ef8c782c7877f53fb4ee45fbf
jiangxs <songxjxs@yeah.net>
2018-04-02 20:01:47 +0800

    修改LeetCode第14题：Longest Common Prefix(easy)

--- src/com/jxs/easy/LongestCommPrefix.java
+++ src/com/jxs/easy/LongestCommPrefix.java
@@ -9,11 +9,18 @@ public class LongestCommPrefix {
 
     public String longestCommonPrefix(String[] strs) {
 
-        System.out.println();
         int length = strs.length;
-        boolean flag = false;
-        String commonPrefix = null;
+        int num = 0;
         StringBuilder common = new StringBuilder();
+
+        if (length == 0) {
+            return "";
+        } else if (length == 1) {
+            return strs[0];
+        }
+
+        // 如果字符串数目不少于2，则设置公共串为前两个的
+        // 如果后面的与公共的相同的更少，将少的代替公共的
         char[] charStr1 = strs[0].toCharArray();
         char[] charStr2 = strs[1].toCharArray();
         int currLength = 0;
@@ -24,31 +31,47 @@ public class LongestCommPrefix {
         }
         for (int i = 0; i < currLength; i++) {
             if (charStr1[i] == charStr2[i]) {
-                char c = charStr1[i];
-                String s = String.valueOf(c);
-                common = common.append(s);
+                common = common.append(String.valueOf(charStr1[i]));
             } else {
                 break;
             }
         }
+        // 如果只有两个字符串，之前的公共部分就是最后所求的公共部分
+        if (length == 2) {
+            return common.toString();
+        }
+        // 字符串数目大于2时
         for (int i = 2; i < length; i++) {
 
+            num = 0;
             char[] chars = strs[i].toCharArray();
             char[] commons = common.toString().toCharArray();
-            for (int j = 0; j < commons.length; j++) {
-                if (commons[j] != chars[j]) {
-                    commonPrefix = common.toString().substring(0, j);
-
+            int cLength = 0;
+            if (chars == null || chars.length == 0) {
+                return "";
+            }
+            if (commons.length <= chars.length) {
+                cLength = commons.length;
+            } else {
+                cLength = chars.length;
+            }
+            for (int j = 0; j < cLength; j++) {
+                if (commons[j] == chars[j]) {
+                    num++;
+                    continue;
                 }
             }
+            // 从不相同的地方开始，后面的字符串全部删除
+            common = common.delete(num,commons.length);
         }
-        return commonPrefix;
+        return common.toString();
     }
 
     @Test
     public void test() {
 
-        String[] strs = {"abcdefg", "abc", "abdc"};
+        String[] strs = {"ac","ac","a","a"};
+
         System.out.println(longestCommonPrefix(strs));
     }
 }
