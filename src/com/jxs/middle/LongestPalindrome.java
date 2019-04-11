e4530c3337d9267acd86b3284ceef6eaf6166e4c
jiangxs <songxjxs@yeah.net>
2019-04-11 17:02:46 +0800

    Longest Palindromic Substring

--- src/com/jxs/middle/LongestPalindrome.java
+++ src/com/jxs/middle/LongestPalindrome.java
@@ -1,73 +1,46 @@
 package com.jxs.middle;
 
 /**
- * Created by jiangxs on 2018/5/21.
+ * Created by jiangxs on 2019/4/11.
  */
 public class LongestPalindrome {
 
     public String longestPalindrome(String s) {
 
         int length = s.length();
-        String substr;
-        StringBuilder str = new StringBuilder();
-
+        if (length == 0) {
+            return "";
+        }
+        int start = 0;
+        int end = 0;
         for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j <= length; j++) {
-                substr = s.substring(i, j);
-                boolean flag = isPalindrome(substr);
-                if (flag) {
-                    str.append(substr);
-                    str.append(",");
-                }
+            int len1 = isPalindrome(s, i, i);
+            int len2 = isPalindrome(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-        String[] strs = str.toString().split(",");
-        String maxStr = "";
-        for (int i = 0; i < strs.length; i++) {
-            if (maxStr.length() <= strs[i].length()) {
-                maxStr = strs[i];
-            }
-        }
-        return maxStr;
+        return s.substring(start, end + 1);
     }
 
-    public boolean isPalindrome(String s) {
+    public int isPalindrome(String s, int left, int right) {
 
-        int length = s.length();
-        int remainder = length % 2;
-        char[] sArray = s.toCharArray();
-        int index = length - 1;
-
-        if (remainder == 0) {
-            for (int i = 0; i <= length / 2; i++) {
-                if (index > (length / 2 - 1) && index > 0 && sArray[i] == sArray[index]) {
-                    index--;
-                    continue;
-                } else {
-                    if (index > (length / 2 - 1) && index > 0) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        } else {
-            for (int i = 0; i <= (length - 1) / 2; i++) {
-                if (index > (length - 1) / 2 && index > 0 && sArray[i] == sArray[index]) {
-                    index--;
-                    continue;
-                } else {
-                    if (index > (length - 1) / 2 && index > 0) {
-                        return false;
-                    }
-                }
-            }
-            return true;
+        char[] strArray = s.toCharArray();
+        int length = strArray.length;
+        int l = left;
+        int r = right;
+        while (l >= 0 && r < length && strArray[l] == strArray[r]) {
+            l--;
+            r++;
         }
+        return r - l - 1;
     }
 
     public static void main(String[] args) {
 
         LongestPalindrome l = new LongestPalindrome();
-        System.out.println(l.longestPalindrome("sss"));
+        System.out.println(l.longestPalindrome(""));
     }
 }
